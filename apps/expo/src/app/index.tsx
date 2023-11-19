08aa81351a3d6ff244a885e7dcd0fdb37cfc26db
HAMEL Julien <julienhamel.h@gmail.com>
2023-11-19 01:51:45 +0100

    fix: typecheck

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -7,20 +7,20 @@ import { FlashList } from "@shopify/flash-list"
 import { api } from "@/utils/api"
 
 const Index = () => {
-  const characters = api.character.all.useQuery()
+  const { data: { characters = [] } = {} } = api.character.all.useQuery({
+    page: 1,
+  })
 
   return (
     <SafeAreaView className="bg-[#1F104A]">
       <Stack.Screen options={{ title: "Home Page" }} />
       <View className="h-full w-full p-4">
-        {characters.data && (
+        {characters && (
           <FlashList
-            data={characters.data}
+            data={characters}
             estimatedItemSize={20}
             ItemSeparatorComponent={() => <View className="h-2" />}
-            renderItem={({ item: character }) => (
-              <Text>{character.fullName}</Text>
-            )}
+            renderItem={({ item: character }) => <Text>{character.name}</Text>}
           />
         )}
       </View>
