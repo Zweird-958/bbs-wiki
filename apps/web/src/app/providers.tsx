8efa53f96cf1d502f8b69ae163595cfad95a62f7
HAMEL Julien <julienhamel.h@gmail.com>
2023-12-20 14:23:26 +0100

    refactor: use vercelUrl and port env

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -8,19 +8,18 @@ import { useState } from "react"
 import superjson from "superjson"
 
 import { api } from "@/utils/api"
+import env from "@/env"
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") {
     return ""
   }
-  // browser should use relative url
 
-  return "http://localhost:3001"
-  // if (env.VERCEL_URL) {
-  //   return env.VERCEL_URL
-  // } // SSR should use vercel url
+  if (env.vercelUrl) {
+    return env.vercelUrl
+  }
 
-  // return `http://localhost:${env.PORT}` // dev SSR should use localhost
+  return `http://localhost:${env.port}`
 }
 
 export function Providers(props: {
--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -3,10 +3,14 @@ import { z } from "zod"
 
 const validationSchema = z.object({
   imagesUrl: imagesUrlValidator,
+  vercelUrl: z.string().optional(),
+  port: z.string().default("3000"),
 })
 
 const env = validationSchema.parse({
   imagesUrl: process.env.NEXT_PUBLIC_IMAGES_URL,
+  vercelUrl: process.env.VERCEL_URL,
+  port: process.env.PORT,
 })
 
 export default env
